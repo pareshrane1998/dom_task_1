13ca52f09ed73fa04171092f667ee39f2789eaba
pareshrane1998 <pareshr32@gmail.com>
2021-05-07 19:32:05 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,32 @@
 
 */
 
+const btns = document.querySelectorAll(".btn");
+// console.log(btns)
+const stroreProducts = document.querySelectorAll(".strore-product") ;
+
+for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener("click",(e) => {
+        e.preventDefault();
+
+        const filter = e.target.dataset.filter;
+        // console.log(filter);
+
+        stroreProducts.forEach((product)=> {
+            if (filter == "all"){
+                product.style.display = "block"
+            } else {
+                if (product.classList.contains(filter)) {
+                    product.style.display = "block"
+                } else {
+                    product.style.display = "none"
+                }
+            }
+
+        } );
+    });
+}
+
 
 
 
